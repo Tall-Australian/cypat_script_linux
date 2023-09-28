7b5e66dc0d23171e18a4dc7c186173ce03d28722
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-09-28 11:51:18 -0400

    Update cypat.sh

--- cypat.sh
+++ cypat.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Create backups
+echo "Creating backups..."
+cat /etc/pam.d/common-password > /etc/pam.d/common-password.bak
+cat /etc/ftpusers > /etc/ftpusers.bak
+cat /etc/ssh/sshd_config > /etc/ssh/sshd_config.bak
+cat /etc/selinux/config > /etc/selinux/config.bak
+
 # Config passwords
+echo "Configuring PAM and updating password policy..."
 apt-get install libpam-cracklib -y
 apt-get install libpam-pwquality -yt
 touch /etc/security/opasswd
@@ -14,11 +22,12 @@ for value in $users
 do
       chage -m 12 -M 90 -W 7 $value
 done
-
 # TODO: rest of pam
-awk -F : '{if ($3<1000) print $1}' /etc/passwd > /etc/ftpusers
+
+rm -f /etc/ftpusers
 
 # ufw
+echo "Installing and configuring ufw..."
 apt-get install ufw -y
 ufw disable
 ufw deny telnet
@@ -29,6 +38,7 @@ chpassd_list=()
 empty=()
 
 # Arg parsing and user adding time
+echo "Manging users and applications..."
 while getopts ":n:p:" o; do
     case "${o}" in
         n)
@@ -57,6 +67,14 @@ while getopts ":n:p:" o; do
             u=${OPTARG}
             apt-get install ${d} -y
             ;;
+        s)
+            # SELinux
+            apt-get install selinux-basics selinux-policy-default auditd -y
+            selinux-activate
+            echo "SELINUX=enforcing" > /etc/selinux/config
+            echo "SELINUXTYPE=strict" >> /etc/selinux/config
+            echo "SELinux has been enabled."
+            ;;
     esac
 done
 
@@ -66,6 +84,37 @@ if [ ! "$chpassd_list" ==  "$empty" ]; then
       done | chpassd
 fi
 
+# Malware protection
+echo "Installing and running malware protection..."
+apt-get install rkhunter -y && rkhunter --propupd && rkhunter -c --skip-keypress
+apt-get install clamav clamav-daemon -y &&
+freshclam &&
+systemctl start clamav-freshclam &&
+clamscan -i -r --remove /
+
+# Write new sshd_config
+echo "Configuring ssh..."
+echo "LogLevel VERBOSE" > /etc/ssh/sshd_config
+echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" >> /etc/ssh/sshd_config
+echo "HostKeyAlgorithms ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-rsa,ssh-dss" >> /etc/ssh/sshd_config
+echo "KexAlgorithms ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha25" >> /etc/ssh/sshd_config
+echo "MACs hmac-sha2-256,hmac-sha2-512,hmac-sha1" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "UsePAM yes" >> /etc/ssh/sshd_config
+echo "AllowTcpForwarding no" >> /etc/ssh/sshd_config
+echo "AllowStreamLocalForwarding no" >> /etc/ssh/sshd_config
+echo "GatewayPorts no" >> /etc/ssh/sshd_config
+echo "PermitTunnel no" >> /etc/ssh/sshd_config
+echo "X11Forwarding no" >> /etc/ssh/sshd_config
+echo "MaxAuthTries 5" >> /etc/ssh/sshd_config
+echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
+echo "HostbasedAuthentication no" >> /etc/ssh/sshd_config
+chown root:root /etc/ssh/sshd_config
+chmod 600 /etc/ssh/sshd_config
+
 # TODO: mas
 
+echo "Updating and restaring..."
 apt-get update -y && apt-get upgrade -y
+read -p "About to reboot, press enter to continue..." dummy
+reboot
