ebc991f6a43e94837b798916bc7aaaebebebab99
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-10-06 19:31:08 -0400

    Update cypat.sh

--- cypat.sh
+++ cypat.sh
@@ -11,23 +11,24 @@ cat /etc/selinux/config > /etc/selinux/config.bak
 echo "Configuring PAM..."
 
 echo "Configuring common-password..."
-apt-get install libpam-cracklib -y
 touch /etc/security/opasswd
-echo "password required pam_cracklib.so retry=3 minlen=12 difok=4" > /etc/pam.d/common-password
-echo "password [success=1 default=ignore] pam_unix.so sha512 remember=5 use_authok shadow" >> /etc/pam.d/common-password
-echo "password requisite pam_deny.so" >> /etc/pam.d/common-password
-echo "password required pam_permit.so" >> /etc/pam.d/common-password
-echo "password optional pam_gnome_keyring.so" >> /etc/pam.d/common-password
+source /etc/os-release
+
+if [ "$ID" == "debian" ]; then
+      apt-get install libpam-cracklib -y
+      echo "#%PAM-1.0" > /etc/pam.d/common-password
+      echo "password required pam_cracklib.so retry=3 minlen=12 difok=4 minclass=4" >> /etc/pam.d/common-password
+      echo "password [success=1 default=ignore] pam_unix.so yescrypt remember=5 use_authok shadow" >> /etc/pam.d/common-password
+      echo "password requisite pam_deny.so" >> /etc/pam.d/common-password
+      echo "password required pam_permit.so" >> /etc/pam.d/common-password
+      echo "password optional pam_gnome_keyring.so" >> /etc/pam.d/common-password
+else
+      echo "difok=4" > /etc/security/pwquality.conf
+      echo "minlen=12" >> /etc/security/pwquality.conf
+      echo "minclass=4" >> /etc/security/pwquality.conf
+fi
 
 echo "Configuring common-auth..."
-# TODO: this
-
-users=$(awk -F: '{ print $1}' /etc/passwd)
-
-for value in $users
-do
-      chage -m 12 -M 90 -W 7 $value
-done
 # TODO: rest of pam
 
 rm -f /etc/ftpusers
@@ -45,7 +46,7 @@ empty=()
 
 # Arg parsing and user adding time
 echo "Manging users and applications..."
-while getopts ":n:p:" o; do
+while getopts ":n:l:d:c:i:u:s:g:" o; do
     case "${o}" in
         n)
             n=${OPTARG}
@@ -81,6 +82,10 @@ while getopts ":n:p:" o; do
             echo "SELINUXTYPE=strict" >> /etc/selinux/config
             echo "SELinux has been enabled."
             ;;
+        g)
+            IFS=':' read -a arr <<< "$line"
+            usermod -aG ${arr[1]} ${arr[0]}
+            ;;
     esac
 done
 
@@ -100,37 +105,8 @@ clamscan -i -r --remove /
 
 # Write new sshd_config
 echo "Configuring ssh..."
-echo "LogLevel VERBOSE" > /etc/ssh/sshd_config
-echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" >> /etc/ssh/sshd_config
-echo "HostKeyAlgorithms ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-rsa,ssh-dss" >> /etc/ssh/sshd_config
-echo "KexAlgorithms ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha25" >> /etc/ssh/sshd_config
-echo "MACs hmac-sha2-256,hmac-sha2-512,hmac-sha1" >> /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "UsePAM yes" >> /etc/ssh/sshd_config
-echo "AllowTcpForwarding no" >> /etc/ssh/sshd_config
-echo "AllowStreamLocalForwarding no" >> /etc/ssh/sshd_config
-echo "GatewayPorts no" >> /etc/ssh/sshd_config
-echo "PermitTunnel no" >> /etc/ssh/sshd_config
-echo "X11Forwarding no" >> /etc/ssh/sshd_config
-echo "MaxAuthTries 5" >> /etc/ssh/sshd_config
-echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
-echo "HostbasedAuthentication no" >> /etc/ssh/sshd_config
-
-echo "Securing important files..."
-chown root:root /boot/efi
-chown root:root /sbin
-chown root:root /bin
-chown root:root /usr
-chown root:root /etc
-chown root:root /var
-chown root:root /lib
-chmod 744 /boot/efi
-chmod 755 /sbin
-chmod 755 /bin
-chmod 755 /usr
-chmod 755 /etc
-chmod 755 /var
-chmod 755 /lib
+# I know not how this works, ChatGPT wrote it for me.
+sed -i -E 's/^#?LogLevel .*/LogLevel VERBOSE/; s/^#?Ciphers .*/Ciphers aes128-ctr,aes192-ctr,aes256-ctr/; s/^#?HostKeyAlgorithms .*/HostKeyAlgorithms ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-rsa,ssh-dss/; s/^#?KexAlgorithms .*/KexAlgorithms ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha25/; s/^#?MACs .*/MACs hmac-sha2-256,hmac-sha2-512,hmac-sha1/; s/^#?PermitRootLogin .*/PermitRootLogin no/; s/^#?UsePAM .*/UsePAM yes/; s/^#?AllowTcpForwarding .*/AllowTcpForwarding no/; s/^#?AllowStreamLocalForwarding .*/AllowStreamLocalForwarding no/; s/^#?GatewayPorts .*/GatewayPorts no/; s/^#?PermitTunnel .*/PermitTunnel no/; s/^#?X11Forwarding .*/X11Forwarding no/' /etc/ssh/sshd_config
 
 # TODO: mas
 
