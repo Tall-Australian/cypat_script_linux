c46913595c24858884b740056068785cda7ef863
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-10-19 10:34:39 -0400

    Update cypat.sh

--- cypat.sh
+++ cypat.sh
@@ -14,19 +14,10 @@ echo "Configuring common-password..."
 touch /etc/security/opasswd
 source /etc/os-release
 
-if [ "$ID" == "debian" ]; then
-      apt-get install libpam-cracklib -y
-      echo "#%PAM-1.0" > /etc/pam.d/common-password
-      echo "password required pam_cracklib.so retry=3 minlen=12 difok=4 minclass=4" >> /etc/pam.d/common-password
-      echo "password [success=1 default=ignore] pam_unix.so yescrypt remember=5 use_authok shadow" >> /etc/pam.d/common-password
-      echo "password requisite pam_deny.so" >> /etc/pam.d/common-password
-      echo "password required pam_permit.so" >> /etc/pam.d/common-password
-      echo "password optional pam_gnome_keyring.so" >> /etc/pam.d/common-password
-else
-      echo "difok=4" > /etc/security/pwquality.conf
-      echo "minlen=12" >> /etc/security/pwquality.conf
-      echo "minclass=4" >> /etc/security/pwquality.conf
-fi
+apt-get install libpam-pwquality -y
+echo "difok=4" > /etc/security/pwquality.conf
+echo "minlen=12" >> /etc/security/pwquality.conf
+echo "minclass=4" >> /etc/security/pwquality.conf
 
 echo "Configuring common-auth..."
 # TODO: rest of pam
@@ -42,7 +33,6 @@ ufw allow ssh # TODO: More detail on protocols
 ufw enable
 
 chpassd_list=()
-empty=()
 
 # Arg parsing and user adding time
 echo "Manging users and applications..."
@@ -52,7 +42,9 @@ while getopts ":n:l:d:c:i:u:s:g:" o; do
             n=${OPTARG}
             IFS=':' read -a arr <<< "$line"
             useradd -m -U ${arr[0]}
-            echo ${arr[1]} | passwd --stdin ${arr[0]}
+        c)
+            c=${OPTARG}
+            chpassd_list+=(${c})
             ;;
         l)
             l=${OPTARG}
@@ -62,17 +54,14 @@ while getopts ":n:l:d:c:i:u:s:g:" o; do
             d=${OPTARG}
             userdel ${OPTARG} # Keep home directory just in case
             ;;
-        c)
-            c=${OPTARG}
-            chpassd_list+=(${c})
-            ;;
+        
         i)
             i=${OPTARG}
             apt-get install ${i} -y
             ;;
         u)
             u=${OPTARG}
-            apt-get install ${d} -y
+            apt-get remove ${d} -y
             ;;
         s)
             # SELinux
@@ -89,11 +78,9 @@ while getopts ":n:l:d:c:i:u:s:g:" o; do
     esac
 done
 
-if [ ! "$chpassd_list" ==  "$empty" ]; then
-      for line in $chpassd_list; do
-          echo "$line"
-      done | chpassd
-fi
+for line in $chpassd_list; do
+      echo "$line"
+done | chpassd
 
 # Malware protection
 echo "Installing and running malware protection..."
@@ -103,8 +90,17 @@ freshclam &&
 systemctl start clamav-freshclam &&
 clamscan -i -r --remove /
 
+echo "Handling common applications..."
+systemctl stop nginx
+apt-get uninstall wireshark
+apt-get uninstall dwarf-fortress
+apt-get uninstall tor
+apt-get uninstall nmap
+
+
 # Write new sshd_config
 echo "Configuring ssh..."
+
 # I know not how this works, ChatGPT wrote it for me.
 sed -i -E 's/^#?LogLevel .*/LogLevel VERBOSE/; s/^#?Ciphers .*/Ciphers aes128-ctr,aes192-ctr,aes256-ctr/; s/^#?HostKeyAlgorithms .*/HostKeyAlgorithms ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-rsa,ssh-dss/; s/^#?KexAlgorithms .*/KexAlgorithms ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha25/; s/^#?MACs .*/MACs hmac-sha2-256,hmac-sha2-512,hmac-sha1/; s/^#?PermitRootLogin .*/PermitRootLogin no/; s/^#?UsePAM .*/UsePAM yes/; s/^#?AllowTcpForwarding .*/AllowTcpForwarding no/; s/^#?AllowStreamLocalForwarding .*/AllowStreamLocalForwarding no/; s/^#?GatewayPorts .*/GatewayPorts no/; s/^#?PermitTunnel .*/PermitTunnel no/; s/^#?X11Forwarding .*/X11Forwarding no/' /etc/ssh/sshd_config
 
